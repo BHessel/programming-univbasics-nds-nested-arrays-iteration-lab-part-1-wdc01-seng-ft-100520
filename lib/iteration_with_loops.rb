ba0165996a7a10e9ac10df933884d8a72362547e
Benjamin Hessel <hesselfitness@gmail.com>
2020-09-15 20:48:43 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,13 +1,13 @@
 def find_even_values(src)
   num = 0 
-  src_even = []
   while num < src.count do
     inside_num = 0
     while inside_num < src[num].count do
       if src[num][inside_num].even?
       p src[num][inside_num]
-      inside_num += 1 
     end
+    inside_num += 1
+  end
     num += 1 
   end
 end
